05e345a4eb21d6704d3241635830f93d1f77ae70
VictorOkpare <victorokpare@gmail.com>
2024-06-11 11:20:26 +0100

    Feat : included CORS

--- rocommerce/settings.py
+++ rocommerce/settings.py
@@ -37,11 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'products',
-    'user'
+    
+    
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -82,6 +85,10 @@ DATABASES = {
     }
 }
 
+
+CORS_ALLOWED_ORIGINS = [
+ "http://localhost:3000"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
--- user/models.py
+++ user/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
-class User(models.Model):
-   first_name = models.CharField(max_length=30)
-   last_name = models.CharField(max_length=30)
-   image= models.ImageField(blank=True)
-   class Meta:
-       ordering = ('name',)
+# class User(models.Model):
+#    first_name = models.CharField(max_length=30)
+#    last_name = models.CharField(max_length=30)
+#    image= models.ImageField(blank=True)
+#    class Meta:
+#        ordering = ('name',)
+#        verbose_name_plural = 'Products'
+
     
 # Create your models here.
